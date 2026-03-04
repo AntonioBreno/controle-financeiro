5502d6315206c46703954f33a0ff11809752e8c6
AntonioBreno <brenogome01@gmail.com>
2026-03-04 15:51:18 -0300

    refactor(categoria): permite null no campo user

--- financas/models/categoria.py
+++ financas/models/categoria.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Categoria(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     
     nome = models.CharField(max_length=100)
     valor_padrao = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
